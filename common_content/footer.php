a2b9136dafb247e8c5ff3dd9ff1dfce0c88a2083
saif foysal <68280552+SaifFoysal00@users.noreply.github.com>
2021-04-20 06:55:57 +0600

    Add files via upload

---
+++ common_content/footer.php
@@ -0,0 +1,50 @@
+<!-- footer top start -->
+<section id="footer_top">
+  <div class="container">
+    <ul class="list-inline cus_ul">
+      <li class="list-inline-item">
+        <a href="#">গোপনীয়তার নীতিমালা</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#">ব্যবহারের শর্তাবলি</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#">সার্বিক সহযোগিতায়</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#">সাইট ম্যাপ</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#">সচরাচর জিজ্ঞাসা</a>
+      </li>
+      
+      
+    </ul>
+  </div>
+</section>
+<!-- footer top end -->
+<!-- footer area start -->
+<section id="footer_wrapper">
+  <div class="container">
+    <div class="cus_con_footer">
+      <div class="row">
+        <div class="col-md-6 col-12">
+          <p>© copyright 2021. natoresadar upazila</p>
+        </div>
+        <div class="col-md-6 col-12">
+          <p class="right_side">Design & Developed by : <a href="">Step Technology</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- footer area end -->
+<!--main jquery -->
+<script src="js/jquery-3.5.1.slim.min.js"></script>
+<!-- bootsrap js -->
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+</body>
+</html>
---
+++ common_content/header.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- favicon css -->
+    <link rel="shortcut icon" type="imgage/png" href="img/favicon.png">
+    <!-- font css -->
+    <link rel="stylesheet" href="font/font.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- custom css -->
+    <!-- all subpage css -->
+    <link rel="stylesheet" href="css/subpage.css">
+	<!--custom css-->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- responsive css  -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <title>নাটোর সদর উপজেলা</title>
+  </head>
+  <body>
+    <!-- top header area start -->
+    <section id="top_header_wrapper">
+      <div class="container head_bg page_shadow">  <!-- container start -->
+      <div class="row">  <!-- row start -->
+      <div class="col-md-5 col-6 slogan cus_col">  <!-- col-md-7 start-->
+      <h4>বাংলাদেশ জাতীয় তথ্য বাতায়ন</h4>
+      </div>  <!-- col-md-7 end-->
+      <div class="col-md-3 col-6 cus_col"> <!-- col-md-2 start-->
+      <div class="dropdown text-right">  <!-- dropdown start -->
+      <a class="btn cus_btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        ইউনিয়ন
+      </a>
+      <div class="dropdown-menu cus_dropdown" aria-labelledby="dropdownMenuLink">
+        <a class="dropdown-item cus_dropdowm_iteam" href="http://natoresadar.com/chhatniup/" target="blank">১ নং ছাতনী</a>
+        <a class="dropdown-item cus_dropdowm_iteam" href="http://natoresadar.com/tebariaup/" target="blank">২ নং তে-বাডিয়া</a>
+        <a class="dropdown-item cus_dropdowm_iteam" href="http://natoresadar.com/dighapatiaup/" target="blank">৩ নং দিঘাপতিয়া</a>
+        <a class="dropdown-item cus_dropdowm_iteam" href="http://natoresadar.com/luxmipurkholabariaup/" target="blank">৪ নং খোলাবাড়িয়া</a>
+        <a class="dropdown-item cus_dropdowm_iteam" href="http://natoresadar.com/barahorispurup/" target="blank">৫ নং বড়-হরিশপুর</a>
+        <a class="dropdown-item cus_dropdowm_iteam" href="http://natoresadar.com/kaphuriaup/" target="blank">৬ নং কাপুরিয়া</a>
+        <a class="dropdown-item cus_dropdowm_iteam" href="http://natoresadar.com/halsa/" target="blank">৭ নং হালসা</a>
+      </div>
+      </div> <!-- dropdown start -->
+      </div> <!-- col-md-2 end-->
+      <div class="col-md-4 d-md-block d-none cus_col"> <!-- col-md-3 start-->
+      <div class="icon_wrap"> <!-- icon_wrap start -->
+      <ul class="list-inline">
+        <li class="list-inline-item">
+          <a href="#">
+            <i class="fa fa-facebook"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#">
+            <i class="fa fa-youtube"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#">
+            <i class="fa fa-envelope"></i>
+          </a>
+        </li>
+        
+      </ul>
+      </div> <!-- icon_wrap end -->
+      </div> <!-- col-md-3 end-->
+      </div> <!-- row end -->
+      </div> <!-- container end -->
+      </section> <!--top header area end-->
+      <!-- menu area start -->
+      <section id="menu_wrapper">
+        <div class="container page_shadow"> <!-- container start -->
+        <nav class="navbar navbar-expand-lg navbar-light cus_nav"> <!-- navbar start -->
+        <!-- toggle button start -->
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button> <!-- toggle button end -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent"> <!-- navbar-collapse start -->
+        <ul class="navbar-nav "> <!-- navbar ul start -->
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php"> <i class="fa fa-home"></i> প্রথম পাথা</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            উপজেলা সম্পর্কিত
+          </a>
+          <div class="dropdown-menu cus_dropdown_menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="upzila_introduce.php">উপজেলা পরিচিতি</a>
+            <a class="dropdown-item" href="upazila_history.php">ইতিহাস ঐতিহ্য</a>
+            <a class="dropdown-item" href="Geographical.php">ভৌগলিক ও অর্থনৈতিক</a>
+            <a class="dropdown-item" href="Public_representative.php">জনপ্রতিনিধিগণের তালিকা</a>
+            <a class="dropdown-item" href="fridomfighter.php">মুক্তিযোদ্ধাদের তালিকা</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            উপজেলা পরিষদ
+          </a>
+          <div class="dropdown-menu cus_dropdown_menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="upzila_chirman.php">চেয়ারম্যান, উপজেলা পরিষদ</a>
+            <a class="dropdown-item" href="upzilavais_chirman.php">ভাইস চেয়ারম্যান</a>
+            <a class="dropdown-item" href="mohilavais_chirman.php">মহিলা ভাইস চেয়ারম্যান</a>
+            <a class="dropdown-item" href="frakton_chirman.php">প্রাক্তন পরিষদ চেয়ারম্যানগণ</a>
+            <a class="dropdown-item" href="upzila_karjaboli.php">উপজেলা পরিষদের কার্যাবল</a>
+            <a class="dropdown-item" href="upzila_sangotonik_katamo.php">সাংগঠনিক কাঠাম</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown ">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            ইউনিয়ন সম্পর্কিত
+          </a>
+          <div class="dropdown-menu cus_dropdown_menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="http://natoresadar.com/chhatniup/" target="blank">১ নং ছাতনী ইউনিয়ন</a>
+            <a class="dropdown-item" href="http://natoresadar.com/tebariaup/" target="blank">২ নং তে-বাডিয়া ইউনিয়ন</a>
+            <a class="dropdown-item" href="http://natoresadar.com/dighapatiaup/" target="blank">৩ নং দিঘাপতিয়া ইউনিয়ন</a>
+            <a class="dropdown-item" href="http://natoresadar.com/luxmipurkholabariaup/" target="blank">৪ নং লক্ষীপুর খোলাবাড়িয়া ইউনিয়ন</a>
+            <a class="dropdown-item" href="http://natoresadar.com/barahorispurup/" target="blank"> ৫ নং বড়-হরিশপুর ইউনিয়ন</a>
+            <a class="dropdown-item" href="http://natoresadar.com/kaphuriaup/" target="blank">  ৬ নং কাপুরিয়া ইউনিয়ন</a>
+            <a class="dropdown-item" href="http://natoresadar.com/halsa/" target="blank">  ৭ নং হালসা ইউনিয়ন</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            পৌরসভা সম্পর্কিত
+          </a>
+          <div class="dropdown-menu cus_dropdown_menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="pourosova.php">এক নজরে পৌরসভা</a>
+            <a class="dropdown-item" href="mayor.php">মেয়র</a>
+            <a class="dropdown-item" href="councilor.php">কাউন্সিলরগণ</a>
+            <a class="dropdown-item" href="pourosova_word.php">ওয়ার্ড সমূহ</a>
+            <a class="dropdown-item" href="pourosova_kormokorta.php">কর্মকর্তাবৃন্দ</a>
+            <a class="dropdown-item" href="pourosova_citizen_serzen.php">সিটিজেন চার্টার</a>
+            <a class="dropdown-item" href="pourosova_kormocari.php">কর্মচারীবৃন্দ</a>
+            <a class="dropdown-item" href="sangotonik_katamo_pourosova.php">সাংগঠনিক কাঠাম</a>
+            <a class="dropdown-item" href="http://bdlaws.minlaw.gov.bd/" target="blank">আইন ও বিধ</a>
+            
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            সরকারি প্রতিষ্ঠান
+          </a>
+          <div class="dropdown-menu cus_dropdown_menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="law_and_order.php">আইন-শৃঙ্খলা বিষয়ক</a>
+            <a class="dropdown-item" href="health_issues.php">স্বাস্থ্য বিষয়ক</a>
+            <a class="dropdown-item" href="agriculture_and_food.php">কৃষি ও খাদ্য বিষয়ক</a>
+            <a class="dropdown-item" href="land_matters.php">ভূমি বিষয়ক</a>
+            <a class="dropdown-item" href="upzila_engineer.php">প্রকৌশল ও যোগাযোগ</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            অন্যান্য প্রতিষ্ঠান
+          </a>
+          <div class="dropdown-menu cus_dropdown_menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="educational_Institutions.php">শিক্ষা প্রতিষ্ঠান</a>
+            <a class="dropdown-item" href="non_governmental_organizations.php">বেসরকারি প্রতিষ্ঠান</a>
+            <a class="dropdown-item" href="religious_institutions.php">ধর্মীয় প্রতিষ্ঠান</a>
+            
+          </div>
+        </li>
+        
+        </ul> <!-- navbar ul end -->
+        
+        </div> <!-- navbar-collpase end -->
+        </nav> <!-- navbar end -->
+        </div> <!-- container end -->
+        
+        </section><!-- menu area end -->
+        <!-- slider section start -->
+        <!-- container start -->
+        <div class="container page_shadow">
+          <!--carousel wrapper -->
+          <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner"> <!--carousel inner start -->
+            <div class="carousel-item active"> <!-- 1st single carousel iteam start -->
+            <img src="img/slider1.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-sm-block">
+              <div class="left_logo">
+                <img src="img/slide_logo.png" class="img-fluid" alt="">
+              </div>
+              <div class="right_text">
+                <h4>নাটোর সদর উপজেলা</h4>
+                <h5>একটি স্বপ্ন একটি দেশ </br> ডিজিটাল বাংলাদেশ</h5>
+              </div>
+            </div>
+            </div><!-- 1st single carousel iteam start -->
+            
+            <div class="carousel-item "> <!-- 2nd single carousel iteam start -->
+            <img src="img/slide2.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-sm-block">
+              <div class="left_logo">
+                <img src="img/slide_logo.png" class="img-fluid" alt="">
+              </div>
+              <div class="right_text">
+                <h4>নাটোর সদর উপজেলা</h4>
+                <h5>একটি স্বপ্ন একটি দেশ </br> ডিজিটাল বাংলাদেশ</h5>
+              </div>
+            </div>
+            </div> <!-- 2nd single carousel iteam start -->
+            
+            </div><!--carousel inner end -->
+            <!-- carousel control start -->
+            <a class="carousel-control-prev cus_slider_control" href="#carouselExampleCaptions" role="button" data-slide="next">
+              <span class="fa fa-angle-double-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next cus_slider_control" href="#carouselExampleCaptions" role="button" data-slide="prev">
+              <span class="fa fa-angle-double-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+            <!-- carousel control end -->
+            </div><!--carousel wrapper end-->
+            </div> <!-- container end -->
+            
+            <!-- slider section end -->
+            <!-- all news marque section start -->
+            <section id="all_news">
+              <div class="container page_shadow">
+                <div class="custom_news_wrap">
+                  <div class="left_news_con">
+                    <i class="fa fa-newspaper-o "></i>
+                    <h4 class="d-md-block d-none"> সকল খবর </h4>
+                    <i class="fa fa-angle-double-right" ></i>
+                  </div>
+                  <div class="right_news_con">
+                    <h5><marquee behavior="" direction="">নো মাস্ক নো সার্ভিস। করোনাভাইরাসের বিস্তার রোধে এখনই ডাউনলোড করুন Corona Tracer BD অ্যাপ। </marquee></h5>
+                  </div>
+                </div>
+              </div>
+              </section> <!-- all news marque section end -->
